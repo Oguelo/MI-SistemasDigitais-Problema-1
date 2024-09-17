6442d465b985e8e36f1dc4c8e800d3990985753d
Luís Eduardo <82675992+duasck@users.noreply.github.com>
2024-09-17 15:56:04 -0300

    testar lib do acelerador

--- accel/accel.c
+++ accel/accel.c
@@ -3,11 +3,50 @@
 #include "physical.h"
 #include <stdio.h>
 
+static unsigned int * i2c0_base_ptr, * sysmgr_base_ptr;
+static void * i2c0base_virtual, * sysmgrbase_virtual;
+static int fd_i2c0base = -1, fd_sysmgr = -1;
+
 int main(void){
 
     uint8_t devid;
     int16_t mg_per_lsb = 4;
     int16_t XYZ[3];
+
+    //Configure MUX to connect I2C0 controller to ADXL345
+	if ((fd_sysmgr = open_physical(fd_sysmgr)) == -1) {
+		return(-1);
+	}
+
+	if ((sysmgrbase_virtual = map_physical(fd_sysmgr, SYSMGR_BASE, SYSMGR_SPAN)) == NULL) {
+		return(-1);
+	}
+
+	sysmgr_base_ptr = (unsigned int *) sysmgrbase_virtual;
+
+    printf("%p\n", sysmgr_base_ptr);
+	printf("Starting mux_init()\n");
+
+	mux_init();
+	printf("Finished mux_init()\n");
+
+
+    if ((fd_i2c0base = open_physical(fd_i2c0base)) == -1) {
+		return(-1);
+	}
+
+	if ((i2c0base_virtual = map_physical(fd_i2c0base, I2C0_BASE, I2C0_SPAN)) == NULL) {
+		return(-1);
+	}
+
+	i2c0_base_ptr = (unsigned int *) i2c0base_virtual;
+	printf("Starting I2C0_Init()\n");
+
+	I2C0_Init();
+	printf("Getting ID\n");
+
+	ADXL345_IdRead(&devid);
+	printf("%#x\n", devid);
     
     // Configure Pin Muxing
     //Pinmux_Config();
